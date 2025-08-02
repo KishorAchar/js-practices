40538d5c761a815257c055fd3b7d265810dd2f77
KishorAchar <kishoreskishores980@gmail.com>
2025-08-02 14:24:45 +0000

    container config

---
+++ 01_basc/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "kishore@google.com"
+var accountPassword = "12345"
+let accountstate;
+
+
+accountCity = "Benglore"
+
+// accountId = 2
+
+accountEmail = "hch@google.com"
+accountPassword = "34555"
+accountCity = "jaipur"
+console.log(accountId);
+/*
+    dont use var 
+    because 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
